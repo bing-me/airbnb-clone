da20be6aef88f091f5b496f36d35410280258111
BingYou Leong <byleong1990@gmail.com>
2023-04-12 16:00:23 +0800

    added bookings routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'flats#index'
   resources :users
-  resources :flats
+  resources :flats do
+    resources :bookings, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

25c21d0b2b2aab11c0b6b0b578320a3729fdcfa8
BingYou Leong <byleong1990@gmail.com>
2023-04-14 11:53:10 +0800

    active record tested

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,2 +1,4 @@
 class VacanciesController < ApplicationController
+  # AR to find value of vacant from both vacancy and flat table
+  #  Vacancy.joins(:flat).where(flat: { country: "USA" }).where(date: Date.new(2023, 6, 1))[0].vacant
 end

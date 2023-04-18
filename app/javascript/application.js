55998a1800334989b8149f23b406102ac4e9b853
BingYou Leong <byleong1990@gmail.com>
2023-04-18 12:42:21 +0800

    added dates and guests to search field

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+document.getElementById("check-out").addEventListener("focus", function() {
+  this.setAttribute("min", document.getElementById("check-in").value);
+});

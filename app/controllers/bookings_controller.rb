1f7939f18d40eb2dc1121ab38a5d1878ea3ee518
BingYou Leong <byleong1990@gmail.com>
2023-04-19 16:38:47 +0800

    added bookings edit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,48 @@
 class BookingsController < ApplicationController
+  before_action :set_flat, only: %i[create]
+  after_action :verify_authorized, except: [:edit, :update]
+
+  def show
+    @booking = Booking.find(booking_params)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.flat = @flat
+    @booking.user = current_user
+
+    authorize @booking
+
+    if @booking.save
+      redirect_to user_path(current_user)
+    else
+      render "flats/show", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    @flat = @booking.flat
+
+    authorize @flat
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @flat = @booking.flat
+
+    authorize @flat
+    @booking.update(booking_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :flat_id)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
 end
